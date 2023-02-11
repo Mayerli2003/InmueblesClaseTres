51c2576594c71a9dcc3c60501add8b90702a0e8b
Mayerli Azucena Montalvo <87994175+Mayerli2003@users.noreply.github.com>
2023-02-11 10:04:22 -0600

    Menu

--- Inmuebles/Inmuebles/Program.cs
+++ Inmuebles/Inmuebles/Program.cs
@@ -7,125 +7,263 @@ using System.Threading.Tasks;
 
 namespace Inmuebles
 {
-    internal class Program
+    class Program
     {
+        
         static void Main(string[] args)
         {
+            
+            int id;
+            Empleado empleado = new Empleado();
+            Cliente cliente = new Cliente();
+            Laboral laboral = new Laboral();
+            Comercial comercial = new Comercial();
+            Civil civil= new Civil();
+            Mina mina = new Mina();
+
+            
+
             Console.WriteLine("----------  BIENVEDIDO/A  ----------");
             Console.WriteLine("MENU:");
             Console.WriteLine("Ingrese el numero de su consulta" +
-               "\n1 - Datos de la compra de inmuble" +
-               "\n2 - Martes" +
-               "\n3 - Miercoles" +
-               "\n4 - Jueves" +
-               "\n5 - Viernes" +
-               "\n6 - Sabado" +
-               "\n7 - Domingo");
-        }
+               "\n1 - Lista de empleados" +
+               "\n2 - Lista de inmuebles"+
+               "\n3 - Datos de la compra de inmubles");
 
+            Console.WriteLine("\nIngrese el numero de su consulta");
+            id = int.Parse(Console.ReadLine());
+            if(id == 1)
+            {
+
+                empleado.nombre();
+            }
+            if (id == 2)
+            {
+
+            }
+            if (id == 3)
+            {
+                Console.WriteLine("----------Datos de la compra de un inmueble----------");
+                Console.WriteLine("\n -Inmueble : " + mina.nombre() +
+                    "\n -Valor monetario: " + mina.valorMonetario()+
+                    "\n -Tipo de inmueble: " + mina.tipo()+
+                    "\n -Tamaño: " + mina.tamaño() +
+                    "\n -Tipo de contrato: " + comercial.descripcion() +
+                    "\n -Duracion del contrato: " + comercial.largoPlazo() +
+                    "\n -Cliente: " + cliente.Cliente1()+
+                    "\n -Empleado: " + empleado.Empleado1() +
+
+                     "\n ------------------------------------------------------- " );
+            }
+            else
+            {
+                Console.WriteLine("El numero de consulta que usted eligio es incorrecto");
+            }
+        }
         abstract class Persona
         {
-            
+
             public void Razonar()
             {
                 Console.WriteLine("Con capacidad de razonar");
             }
 
             public abstract void nombre();
-           
+
 
         }
-        class Cliente: Persona
+        class Empleado: Persona
         {
-            Comercial contratoComercial = new Comercial();
+            Laboral laboral = new Laboral();
+            public override void nombre()
+            {
+
+
+                string[] nombreEmpleado = new string[10];
+                nombreEmpleado[0] = "Matias Castilllo";
+                nombreEmpleado[1] = "Rigoberto Montalvo";
+                nombreEmpleado[2] = "Maria Monterrosa";
+                nombreEmpleado[3] = "Judith Martínez";
+                nombreEmpleado[4] = "Marta Caballero";
+                nombreEmpleado[5] = "Ricardo Guardado";
+                nombreEmpleado[6] = "Marbely Martínez";
+                nombreEmpleado[7] = "Roberto Fuentes";
+                nombreEmpleado[8] = "Margarita Madrid";
+                nombreEmpleado[9] = "Dania Herrera";
+                
+                Console.WriteLine("Lista de empleados:\n" +
+                    "\n1-" + nombreEmpleado[0] +
+                    "\n -Tipo de Contrato: " + laboral.descripcion()+
+                    "\n -Duracion: " + laboral.medianoPlazo()
+                    +"\n ------------------------------------------------------- "+
+                    "\n2-" + nombreEmpleado[1]+
+                    "\n3-" + nombreEmpleado[2]+
+                    "\n4-" + nombreEmpleado[3] +
+                    "\n5-" + nombreEmpleado[4] +
+                    "\n6-" + nombreEmpleado[5]+
+                    "\n7-" + nombreEmpleado[6] +
+                    "\n8-" + nombreEmpleado[7] +
+                    "\n9-" + nombreEmpleado[8] +
+                    "\n10-" + nombreEmpleado[9]);
+
+                
+            }
+            public string Empleado1()
+            {
+                return ("Matias Castilllo");
+            }
+        }
+
+        class Cliente : Persona
+        {
+
+            Comercial comercial = new Comercial();
             
 
             public override void nombre()
             {
 
                 string[] nombreCliente = new string[10];
-                nombreCliente[0] = "Matias Castilllo";
-                nombreCliente[1] = "Rigoberto Montalvo";
-                nombreCliente[2] = "Maria Monterrosa";
-                nombreo[3] = "Judith Martíz";
-                nombreEmpleado[4] = "Marta Caballero";
-                nombreEmpleado[5] = "Ricardo Guardado";
-                nombreEmpleado[6] = "Marbely Martínez";
-                nombreEmpleado[7] = "Roberto Fuentes";
-                nombreEmpleado[8] = "Margarita Madrid";
-                nombreEmpleado[9] = "Dania Herrera";
+                nombreCliente[0] = "Bilma Aguilar";
+                nombreCliente[1] = "Lili Montezino";
+                nombreCliente[2] = "Aracely Mendoza";
+                nombreCliente[3] = "Abel Serrano";
+                nombreCliente[4] = "Dayana Nuñez";
+                nombreCliente[5] = "Nicolle Chanchan";
+                nombreCliente[6] = "Damaris Ayala";
+                nombreCliente[7] = "Marcela Perez";
+                nombreCliente[8] = "Cristina Lopez";
+                nombreCliente[9] = "Isabel Menjivar";
+
+                Console.WriteLine("Lista de empleados:\n" +
+                    "\n1-" + nombreCliente[0] +
+                    "\n -Tipo de Contrato: " + comercial.descripcion() +
+                    "\n -Duracion: " + comercial.cortoPlazo()
+                    + "\n ------------------------------------------------------- " +
+                    "\n2-" + nombreCliente[1] +
+                    "\n3-" + nombreCliente[2] +
+                    "\n4-" + nombreCliente[3] +
+                    "\n5-" + nombreCliente[4] +
+                    "\n6-" + nombreCliente[5] +
+                    "\n7-" + nombreCliente[6] +
+                    "\n8-" + nombreCliente[7] +
+                    "\n9-" + nombreCliente[8] +
+                    "\n10-" + nombreCliente[9]);
 
             }
-            
-        }
-        class Empleado : Persona
-        {
-            Laboral contratoLaboral = new Laboral();
-            
-
-            public override void nombre()
+            public string Cliente1()
             {
-                string[] nombreEmpleado= new string[10];
-                nombreEmpleado[0] = "Matias Castilllo";
-                nombreEmpleado[1] = "Rigoberto Montalvo";
-                nombreEmpleado[2] = "Maria Monterrosa";
-                nombreEmpleado[3] = "Judith Martíz";
-                nombreEmpleado[4] = "Marta Caballero";
-                nombreEmpleado[5] = "Ricardo Guardado";
-                nombreEmpleado[6] = "Marbely Martínez";
-                nombreEmpleado[7] = "Roberto Fuentes";
-                nombreEmpleado[8] = "Margarita Madrid";
-                nombreEmpleado[9] = "Dania Herrera";
+                return ("Bilma Aguilar");
 
             }
 
-            
+
+
         }
-        
+
+
         abstract class Inmueble
         {
-            public abstract void valorMonetario();
+            public abstract string tamaño();
+            public abstract double valorMonetario();
+            public abstract string tipo();
+
+            public abstract string nombre();
             
-            public abstract void durabilidad();
-
-            //public string heterogeneidad;
-            //public string localizacion;
-
-
 
         }
-       
+        class Mina: Inmueble
+        {
+            public override string tamaño()
+            {
+                return("1000 metros cuadrados");
+            }
+            public override double valorMonetario()
+            {
+                return(2000000);
+            }
+            public override string tipo()
+            {
+                return("Bien inmueble por naturaleza");
+            }
+            public override string nombre()
+            {
+                return("Carlin y Goldstrike");
+            }
+
+        }
+        
+
         abstract class Contrato
         {
-            public string duracion;
-            public string contenido;
-            public string forma;
-            public string interpretacion;
-            public string eficacia;
+            public abstract string cortoPlazo();
+            public abstract string medianoPlazo();
+            public abstract string largoPlazo();
+            public abstract string descripcion();
+            
+
 
         }
         class Laboral : Contrato
         {
+            public override string cortoPlazo()
+            {
+                return("Tres meses de duracion");
+            }
+            public override string medianoPlazo()
+            {
+                return("Un año");
+            }
+            public override string largoPlazo()
+            {
+                return("Tres años");
+            }
+            public override string descripcion()
+            {
+                return("Contrato laboral");
+            }
 
         }
-        class  Civil: Contrato
+        class Civil : Contrato
         {
+            public override string cortoPlazo()
+            {
+                return ("Seis meses de duracion");
+            }
+            public override string medianoPlazo()
+            {
+                return ("Un año y medio");
+            }
+            public override string largoPlazo()
+            {
+                return ("Dos años");
+            }
+            public override string descripcion()
+            {
+                return ("Contrato civil");
+            }
+
 
         }
         class Comercial : Contrato
         {
+            public override string cortoPlazo()
+            {
+                return ("Dos meses de duracion");
+            }
+            public override string medianoPlazo()
+            {
+                return ("Seis meses");
+            }
+            public override string largoPlazo()
+            {
+                return ("Dos años");
+            }
+            public override string descripcion()
+            {
+                return ("Contrato comercial");
+            }
 
         }
-        //Crear un menu con la opciones
-        //que permita elegir entre el tipo de mobiliario
-        //Bienes inmuebles por naturaleza. ...
-        //Bienes inmuebles por incorporación. ...
-        //Bienes inmuebles por destino. ...
-        //Bienes inmuebles por analogía
-        //al elegir uno qque le de dos opciones una respuesta de si y no
-        //si dice que no que lo retorne al menu 
-        // si dice que si que muestre lo siguiente
-        // que muestre el empleado y cliente que compro el inmueble asi como 
-
     }
 }
